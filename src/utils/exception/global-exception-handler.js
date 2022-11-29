cc8bf04bcc3ba573bf1680f8bc4f71f6bcb8c59e
kyoohyun <kgh2120@naver.com>
2022-11-29 15:44:58 +0900

    CHORE : globalExceptionHandler() 메서드 이름 변경 -> handleConnectionError

--- src/utils/exception/global-exception-handler.js
+++ src/utils/exception/global-exception-handler.js
@@ -1,6 +1,6 @@
 import {SERVER_CONNECTION_ERROR} from "./global-error-message";
 
-export const globalExceptionHandler = (errMessage) => {
+export const handleConnectionError = (errMessage) => {
     switch (errMessage) {
         case SERVER_CONNECTION_ERROR.errorMessage :
             SERVER_CONNECTION_ERROR.response();
@@ -12,5 +12,5 @@ export const handleError = (error) => {
     if (error.response?.data.errorMessage !== undefined)
         alert(error.response.data.errorMessage);
     else
-        globalExceptionHandler(error.message);
+        handleConnectionError(error.message);
 }
